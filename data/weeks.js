92730737f5d0469483f7172cfdbded93bfff5385
Visayacode <ronmortega1@gmail.com>
2026-04-17 19:08:54 +0800

    new updates on UI and data

--- data/weeks.js
+++ data/weeks.js
@@ -6,6 +6,7 @@ const DATA = {
       date: "Feb 23 - Feb 27, 2026",
       desc: "On this day, I collected and reviewed detailed enrollment and graduation data for the Open University. This involved gathering annual figures categorized by program to ensure a comprehensive dataset for analysis. Additionally, historical information from the establishment of the Open University in 1997 up to the present was researched and organized. The data collected will serve as a foundation for identifying trends, preparing summaries, and supporting the creation of visualizations and reports.",
       img: "/images/kaon.jpg",
+      wfh: "hahaha",
 
       dtr: {
         mon: { am: "8:20-12:00", pm: "1:30-5:00" },
@@ -74,15 +75,131 @@ const DATA = {
         "/images/profile.jpg"
       ]
     },
+    {
+      id: "week4",
+      title: "Week 4",
+      date: "Mar 16 - Mar 21, 2026",
+      desc: "On this day, I collected and reviewed detailed enrollment and graduation data for the Open University. This involved gathering annual figures categorized by program to ensure a comprehensive dataset for analysis. Additionally, historical information from the establishment of the Open University in 1997 up to the present was researched and organized. The data collected will serve as a foundation for identifying trends, preparing summaries, and supporting the creation of visualizations and reports.",
+      img: "/images/kaon.jpg",
+
+      dtr: {
+        mon: { am: "8:20-12:00", pm: "1:30-5:00" },
+        tue: { am: "8:30-12:00", pm: "1:20-5:00" },
+        wed: { am: "8:40-12:00", pm: "1:20-5:00" },
+        thu: { am: "8:50-12:00", pm: "1:20-5:00" },
+        fri: "WFH",
+        sat: "WFH"
+      },
+
+      images: [
+        "/images/kaon.jpg",
+        "/images/profile1.jpg",
+        "/images/profile2.jpg",
+        "/images/profile.jpg",
+        "/images/profile.jpg"
+      ]
+    },
+    {
+      id: "week5",
+      title: "Week 5",
+      date: "Mar 23 - Mar 28, 2026",
+      desc: "On this day, I collected and reviewed detailed enrollment and graduation data for the Open University. This involved gathering annual figures categorized by program to ensure a comprehensive dataset for analysis. Additionally, historical information from the establishment of the Open University in 1997 up to the present was researched and organized. The data collected will serve as a foundation for identifying trends, preparing summaries, and supporting the creation of visualizations and reports.",
+      img: "/images/kaon.jpg",
+
+      dtr: {
+        mon: { am: "8:20-12:00", pm: "1:30-5:00" },
+        tue: { am: "8:30-12:00", pm: "1:20-5:00" },
+        wed: { am: "8:40-12:00", pm: "1:20-5:00" },
+        thu: { am: "8:50-12:00", pm: "1:20-5:00" },
+        fri: "WFH",
+        sat: "WFH"
+      },
+
+      images: [
+        "/images/kaon.jpg",
+        "/images/profile1.jpg",
+        "/images/profile2.jpg",
+        "/images/profile.jpg",
+        "/images/profile.jpg"
+      ]
+    },
+    {
+      id: "week6",
+      title: "Week 6",
+      date: "Mar 30 - Apr 1, 2026",
+      desc: "On this day, I collected and reviewed detailed enrollment and graduation data for the Open University. This involved gathering annual figures categorized by program to ensure a comprehensive dataset for analysis. Additionally, historical information from the establishment of the Open University in 1997 up to the present was researched and organized. The data collected will serve as a foundation for identifying trends, preparing summaries, and supporting the creation of visualizations and reports.",
+      img: "/images/kaon.jpg",
+
+      dtr: {
+        mon: { am: "8:20-12:00", pm: "1:30-5:00" },
+        tue: { am: "8:30-12:00", pm: "1:20-5:00" },
+        wed: { am: "8:40-12:00", pm: "1:20-5:00" },
+        thu: { am: "8:50-12:00", pm: "1:20-5:00" },
+        fri: "WFH",
+        sat: "WFH"
+      },
+
+      images: [
+        "/images/kaon.jpg",
+        "/images/profile1.jpg",
+        "/images/profile2.jpg",
+        "/images/profile.jpg",
+        "/images/profile.jpg"
+      ]
+    },
   ],
 
   APR: [
-    {
-      title: "Week 1",
-      date: "Apr 1 - Apr 5, 2026",
-      desc: "Continued administrative assistance and record verification.",
-      img: "/images/kaon.jpg"
-    }
+   {
+      id: "week7",
+      title: "Week 7",
+      date: "Apr 6 - Apr 10, 2026",
+      desc: "On this day, I collected and reviewed detailed enrollment and graduation data for the Open University. This involved gathering annual figures categorized by program to ensure a comprehensive dataset for analysis. Additionally, historical information from the establishment of the Open University in 1997 up to the present was researched and organized. The data collected will serve as a foundation for identifying trends, preparing summaries, and supporting the creation of visualizations and reports.",
+      img: "/images/kaon.jpg",
+      wfh: "hahaha",
+
+      dtr: {
+        mon: { am: "8:20-12:00", pm: "1:30-5:00" },
+        tue: { am: "8:30-12:00", pm: "1:20-5:00" },
+        wed: { am: "8:40-12:00", pm: "1:20-5:00" },
+        thu: { am: "8:50-12:00", pm: "1:20-5:00" },
+        fri: "WFH",
+        sat: "WFH"
+      },
+
+      images: [
+        "/images/kaon.jpg",
+        "/images/profile1.jpg",
+        "/images/profile2.jpg",
+        "/images/profile.jpg",
+        "/images/profile.jpg"
+      ]
+    },
+       {
+      id: "week8",
+      title: "Week 8",
+      date: "Apr 13 - Apr 18, 2026",
+      desc: "During the week, I updated and pushed the latest design of the directory page to GitHub. I also assisted a staff member in completing tasks in the Bicol University Student Archiving System (BUSAS), specifically helping finalize assigned Transcript of Records (TOR) entries. Additionally, I handled document filing, delivered and facilitated the receiving of documents between offices such as the OVPA and Supply Office, and scanned various documents, including TORs and other related files.",
+      img: "/images/kaon.jpg",
+      wfh: "nag busas",
+
+      dtr: {
+        mon: { am: "8:00-12:00", pm: "1:00-5:00" },
+        tue: { am: "8:00-12:00", pm: "1:00-5:00" },
+        wed: { am: "8:00-12:00", pm: "1:00-5:00" },
+        thu: { am: "8:00-12:00", pm: "1:00-5:00" },
+        fri: "WFH",
+        sat: "WFH"
+      },
+
+      images: [
+        "/images/week-8/week8.jpg",
+        "/images/week-8/week8.1.jpg",
+        "/images/week-8/week8.2.jpg",
+        "/images/week-8/week8.3.jpg",
+         "/images/week-8/wee8.4.jpg",
+      ]
+    },
   ],
 
   MAY: [
