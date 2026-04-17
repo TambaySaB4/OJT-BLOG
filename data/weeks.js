5e0deb73d44cda237814434ab1d4bd4dd0d9aa0f
Visayacode <ronmortega1@gmail.com>
2026-04-17 23:08:47 +0800

    new DTR UI and data

--- data/weeks.js
+++ data/weeks.js
@@ -26,52 +26,53 @@ const DATA = {
     },
   ],
   MAR: [
-     {
+      {
       id: "week2",
       title: "Week 2",
       date: "Mar 2 - Mar 6, 2026",
-      desc: "On this day, I collected and reviewed detailed enrollment and graduation data for the Open University. This involved gathering annual figures categorized by program to ensure a comprehensive dataset for analysis. Additionally, historical information from the establishment of the Open University in 1997 up to the present was researched and organized. The data collected will serve as a foundation for identifying trends, preparing summaries, and supporting the creation of visualizations and reports.",
-      img: "/images/profile1.jpg",
+      desc: "Spent the entire week systematically scanning and organizing student records, particularly focusing on the 2019 graduates’ files. This involved carefully sorting physical documents into three categories: Permanent Records, Official Transcript of Records (OTR), and Draft OTRs. The week-long task ensured that all records from the 2019 batch were properly organized for both digital backup and physical filing, maintaining accuracy and consistency within the office’s archival system for efficient long-term retrieval.",
+      img: "/images/week-2/week2.jpg",
+      wfh: "",
 
       dtr: {
-        mon: { am: "8:20-12:00", pm: "1:30-5:00" },
-        tue: { am: "8:30-12:00", pm: "1:20-5:00" },
-        wed: { am: "8:40-12:00", pm: "1:20-5:00" },
-        thu: { am: "8:50-12:00", pm: "1:20-5:00" },
-        fri: "WFH",
-        sat: "WFH"
-      },
+       mon: { type: "office", am: "7:54-12:01", pm: "12:54-05:10" },
+       tue: { type: "office", am: "7:58-12:06", pm: "12:33-5:10" },
+       wed: { type: "office", am: "7:37-12:18", pm: "12:34-5:29" },
+       thu: { type: "office", am: "7:43-12:25", pm: "12:37-5:09" },
+       fri: { type: "office", am: "7:35-12:25", pm: "12:33-5:10" },
+          },
 
       images: [
-        "/images/kaon.jpg",
-        "/images/profile1.jpg",
-        "/images/profile2.jpg",
-        "/images/profile.jpg",
-        "/images/profile.jpg"
+        "/images/week-2/week2.jpg",
+        "/images/week-2/week2.4.jpg",
+        "/images/week-2/week2.3.jpg",
+        "/images/week-2/week2.2.jpg",
+        "/images/week-2/week2.1.jpg",
       ]
     },
-     {
+    {
       id: "week3",
       title: "Week 3",
       date: "Mar 9 - Mar 14, 2026",
-      desc: "On this day, I collected and reviewed detailed enrollment and graduation data for the Open University. This involved gathering annual figures categorized by program to ensure a comprehensive dataset for analysis. Additionally, historical information from the establishment of the Open University in 1997 up to the present was researched and organized. The data collected will serve as a foundation for identifying trends, preparing summaries, and supporting the creation of visualizations and reports.",
-      img: "/images/kaon.jpg",
+      desc: "Conducted the alpha listing of student records to create a more efficient and searchable database for the office by organizing student names alphabetically and verifying their corresponding academic information for proper categorization. This improved the accuracy and accessibility of student file retrieval and supported better record management. I also delivered documents to OSAS. Our mentor also assisted us in setting up the project environment. I worked on the UI/UX design phase for the new University Registrar’s Office (URO) website using Figma as part of the initial design process, as well as the UI/UX design for the directory and FAQs pages. We also underwent orientation on how to use the BUSAS system.",
+      img: "/images/week-3/week3.3.jpg",
+      wfh: "During WFH, I worked on the BUSAS system by adding academic curricula into the system and I began coding the UI for the FAQs and directory pages.",
 
       dtr: {
-        mon: { am: "8:20-12:00", pm: "1:30-5:00" },
-        tue: { am: "8:30-12:00", pm: "1:20-5:00" },
-        wed: { am: "8:40-12:00", pm: "1:20-5:00" },
-        thu: { am: "8:50-12:00", pm: "1:20-5:00" },
-        fri: "WFH",
-        sat: "WFH"
-      },
+       mon: { type: "office", am: "7:39-12:26", pm: "12:34-05:10" },
+       tue: { type: "office", am: "7:43-12:38", pm: "12:42-5:13" },
+       wed: { type: "office", am: "7:45-12:17", pm: "12:23-5:15" },
+       thu: { type: "office", am: "7:45-12:25", pm: "12:31-5:10" },
+       fri: { type: "wfh"},
+       sat: { type: "wfh"},
+          },
 
       images: [
-        "/images/kaon.jpg",
-        "/images/profile1.jpg",
-        "/images/profile2.jpg",
-        "/images/profile.jpg",
-        "/images/profile.jpg"
+        "/images/week-3/week3.3.jpg",
+        "/images/week-3/week3.jpg",
+        "/images/week-3/week3.2.jpg",
+        "/images/week-3/week3.4.jpg",
+        "/images/week-3/week3.1.jpg",
       ]
     },
     {
